128b67efae25f296233f6c1da17ceb54da07dffa
Yunus Chang <yunus@s-169-232-122-15.resnet.ucla.edu>
2018-09-30 13:54:05 -0700

    Initial Commit

---
+++ Practice 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Practice 1
+//
+//  Created by Yunus Chang on 9/30/18.
+//  Copyright © 2018 Yunus Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
