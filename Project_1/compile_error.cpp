87334f271fe233be0a057d53981cf3187fb5705a
YunusC <43123784+YunusC@users.noreply.github.com>
2018-10-09 23:33:24 -0700

    Project 1

---
+++ Project_1/compile_error.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  compile_error
+//
+//  Created by Yunus Chang on 10/2/18.
+//  Copyright © 2018 Yunus Chang. All rights reserved.
+// Code for Project 1
+// Report poll results
+
+#include <iostream>
+using namespace std;   // pp. 38-39 in Savitch 6/e explains this line
+
+int main()
+{
+    int numberSurveyed;
+    int forNewsom;
+    forCox; //removed the intialization
+    
+    cout << How many registered voters were surveyed? ;//removed quotation marks
+    cin >> numberSurveyed;
+    cout << "How many of them say they will vote for Newsom? ";
+    cin >> forNewsom;
+    cout << "How many of them say they will vote for Cox? ";
+    cin >> forCox;
+    
+    double pctNewsom = 100.0 * forNewsom / numberSurveyed;
+    double pctCox = 100.0 * forCox / numberSurveyed;
+    
+    cout.setf(ios::fixed);       // see pp. 32-33 in Savitch 6/e
+    cout.precision(1);
+    
+    cout << endl;
+    cout << pctNewsom << "% say they will vote for Newsom." << endl;
+    cout << pctCox << "% say they will vote for Cox." << endl;
+    
+    if (forNewsom > forCox)
+        cout << "Newsom is predicted to win the election." << endl;
+    else
+        cout << "Cox is predicted to win the election." << endl;
+}
